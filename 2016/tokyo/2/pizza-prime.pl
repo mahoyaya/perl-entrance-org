e37bc1c41c96166c1631ed477e41c4e93fc7fdb0
mahoyaya <snakefighting@gmail.com>
2016-06-14 23:34:29 +0900

    配列操作をやめた

--- 2016/tokyo/2/pizza-prime.pl
+++ 2016/tokyo/2/pizza-prime.pl
@@ -7,21 +7,23 @@ my @prime = ();
 sub is_prime {
   my $n = shift;
   for my $p (@prime) { return 0 if $n % $p == 0; }
-  my $start;
-  scalar @prime > 0 ? $start = $prime[$#prime] : $start = 2;
+  my $start = 2;
+  #scalar @prime > 0 ? $start = $prime[$#prime] : $start = 2;
+  #scalar @prime > 0 ? $start = $prime[0] : $start = 2;
   for (my $i = $start; $i <= $n/2; $i++){
     return 0 if $n % $i == 0;
   }
   return 1;
 }
 
-my $max = 1000;
+my $max = 10000;
 for my $n (2..$max) {
   if (is_prime($n)) {
-    say "Pizza";
+    #say "Pizza";
   } else {
-   say $n;
-   push(@prime, $n);
+    #say $n;
+    #push(@prime, $n);
+    #unshift(@prime, $n);
   }
 }
 
