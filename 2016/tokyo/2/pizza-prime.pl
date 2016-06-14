2d63c69b41492b7ecf78049abf1679e636d65c81
mahoyaya <snakefighting@gmail.com>
2016-06-14 23:05:53 +0900

    pizza-prime.pl作成

---
+++ 2016/tokyo/2/pizza-prime.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use feature 'say';
+
+my @prime = ();
+sub is_prime {
+    my $n = shift;
+    for my $p (@prime) { return 0 if $n % $p == 0; };
+    my $start;
+    scalar @prime > 0 ? $start = $prime[$#prime] : $start = 2;
+    for (my $i = $start; $i <= $n/2; $i++){
+        return 0 if $n % $i == 0;
+    }
+    return 1;
+}
+
+my $max = 1000;
+for my $n (2..$max) {
+    is_prime($n) ? say "Pizza" : say $n;
+}
+
+exit 0;
