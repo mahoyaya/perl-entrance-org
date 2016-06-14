d06ef334d6d3f4f50f1f96cb8ad3030fe702158f
mahoyaya <snakefighting@gmail.com>
2016-06-14 23:24:14 +0900

    @primeにpushする様に修正

--- 2016/tokyo/2/pizza-prime.pl
+++ 2016/tokyo/2/pizza-prime.pl
@@ -5,19 +5,24 @@ use feature 'say';
 
 my @prime = ();
 sub is_prime {
-    my $n = shift;
-    for my $p (@prime) { return 0 if $n % $p == 0; }
-    my $start;
-    scalar @prime > 0 ? $start = $prime[$#prime] : $start = 2;
-    for (my $i = $start; $i <= $n/2; $i++){
-        return 0 if $n % $i == 0;
-    }
-    return 1;
+  my $n = shift;
+  for my $p (@prime) { return 0 if $n % $p == 0; }
+  my $start;
+  scalar @prime > 0 ? $start = $prime[$#prime] : $start = 2;
+  for (my $i = $start; $i <= $n/2; $i++){
+    return 0 if $n % $i == 0;
+  }
+  return 1;
 }
 
 my $max = 1000;
 for my $n (2..$max) {
-    is_prime($n) ? say "Pizza" : say $n;
+  if (is_prime($n)) {
+    say "Pizza";
+  } else {
+   say $n;
+   push(@prime, $n);
+  }
 }
 
 exit 0;
